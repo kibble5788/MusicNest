2db97bbc23cd1ef681ba87f85b19fd7b4a437f71
kibble5788 <kibble5788@gmail.com>
2025-04-21 11:21:20 +0800

    更新配置文件，设置 assetPrefix、basePath 和 trailingSlash；更改项目名称为 music-h5；修订 README.md 文件，简化描述并移除多余内容；移除 layout.tsx 中未使用的 Head 导入。

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { MusicProvider } from "@/context/music-context"
 import { AuthProvider } from "@/context/auth-context"
-import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
